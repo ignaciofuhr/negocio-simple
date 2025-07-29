89fcf74c857d4ba2c29bec4dbee71b0e13dff555
ignaciofuhr <ignaciofuhr99@gmail.com>
2025-07-29 13:29:04 +0200

    grego workflow para build y deploy automático en Render

--- src/main/java/com/softcon/negociosimple/TestController.java
+++ src/main/java/com/softcon/negociosimple/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 @GetMapping("/ping")
     public String ping(){
-    return "pong";
+    return "Micaela";
 }
 }
