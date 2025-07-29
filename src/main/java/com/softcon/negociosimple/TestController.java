6c9d63df34c0ede855ad4dac0ce6d6f46a79b19e
ignaciofuhr <ignaciofuhr99@gmail.com>
2025-07-29 14:03:57 +0200

    Test - Build + CI/CD

--- src/main/java/com/softcon/negociosimple/TestController.java
+++ src/main/java/com/softcon/negociosimple/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 @GetMapping("/ping")
     public String ping(){
-    return "Micaela";
+    return "Micaela e Ignacio (L)";
 }
 }
